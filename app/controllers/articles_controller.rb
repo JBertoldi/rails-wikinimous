1126aa283e7e634c01d6d7b5fec59c7c71cf5738
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-03 23:03:23 +0000

    Added show and index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,31 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :delete]
+  
   def index
-    @articles.all
+    @articles = Article.all
   end
 
   def show
   end
 
-  def create
+  def new
+    @article = Article.new
   end
 
-  def new
+  def create
+    article = Article.create(article_params)
+    redirect_to articles_path
   end
 
   def edit
   end
 
   def update
+    @article.update(article_params)
   end
 
   def delete
+    @article.destroy
   end
 
   private
@@ -27,5 +33,9 @@ class ArticlesController < ApplicationController
   def set_article
     @article = Article.find(params[:id])
   end
-  
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/create'
-  get 'articles/new'
-  get 'articles/edit'
-  get 'articles/update'
-  get 'articles/delete'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles, :except => [:show]
+
+  get 'articles/:id', to: 'articles#show', as: :article_show
 end
